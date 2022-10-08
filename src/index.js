ed2f366e2ab8ea5854164d61ae4034be507227d5
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-10-08 20:16:00 +0200

    adjust errors that did not passed in the automated tests

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-const repositories = [];
+const repositories = [{
+  id: uuid(),
+  title: "Movie-app",
+  url: "https://movie.app",
+  techs: ["javascript", "nodejs"],
+  likes: 0
+}];
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -23,19 +29,21 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository)
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
+  updatedRepository.likes = repositories[repositoryIndex].likes
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
   repositories[repositoryIndex] = repository;
@@ -48,7 +56,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +74,9 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes++;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
